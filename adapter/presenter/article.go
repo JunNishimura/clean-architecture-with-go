628c659293106f8193ef80284ed23f57a70b4802
Jun Nishimura <n.junjun0303@gmail.com>
2023-09-23 01:23:15 +0900

    update adapter layer (#17)

--- adapter/presenter/article.go
+++ adapter/presenter/article.go
@@ -1,9 +1,12 @@
 package presenter
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
-	"github.com/JunNishimura/clean-architecture-with-go/entities"
 	"github.com/JunNishimura/clean-architecture-with-go/usecase/port"
 )
 
@@ -17,8 +20,26 @@ func NewArticle(w http.ResponseWriter) port.ArticleOutput {
 	}
 }
 
-func (a *article) Render([]*entities.Article) {
+type ErrResponse struct {
+	Message string   `json:"message"`
+	Details []string `json:"details,omitempty`
 }
 
-func (a *article) RenderError(error) {
+func (a *article) Render(ctx context.Context, body any, status int) {
+	a.w.Header().Set("Content-Type", "applications/json; charset=utf-8")
+	bodyBytes, err := json.Marshal(body)
+	if err != nil {
+		a.w.WriteHeader(http.StatusInternalServerError)
+		rsp := ErrResponse{
+			Message: http.StatusText(http.StatusInternalServerError),
+		}
+		if err := json.NewEncoder(a.w).Encode(rsp); err != nil {
+			log.Printf("fail to write response error: %v", err)
+		}
+		return
+	}
+	a.w.WriteHeader(status)
+	if _, err := fmt.Fprintf(a.w, "%s", bodyBytes); err != nil {
+		log.Printf("fail to write response: %v", err)
+	}
 }
