c61874e8e4630c85e5487de0e5027fca8516afa4
Jun Nishimura <n.junjun0303@gmail.com>
2023-09-24 20:38:15 +0900

    add delete to usecase (#23)

--- usecase/repository/article.go
+++ usecase/repository/article.go
@@ -10,4 +10,5 @@ type Article interface {
 	FindAll(context.Context) ([]*entities.Article, error)
 	FindByID(context.Context, int64) (*entities.Article, error)
 	Create(context.Context, *entities.Article) error
+	Delete(context.Context, int64) error
 }
