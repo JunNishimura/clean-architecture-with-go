1128e7745712e7b3d4298e2e5b4f2d87d4a03953
Jun Nishimura <n.junjun0303@gmail.com>
2023-09-24 21:24:55 +0900

    update usecase (#25)

--- usecase/repository/article.go
+++ usecase/repository/article.go
@@ -10,5 +10,6 @@ type Article interface {
 	FindAll(context.Context) ([]*entities.Article, error)
 	FindByID(context.Context, int64) (*entities.Article, error)
 	Create(context.Context, *entities.Article) error
+	Update(ctx context.Context, articleID int64, title, body *string) error
 	Delete(context.Context, int64) error
 }
