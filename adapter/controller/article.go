01955ff9e90f2faa590708af2340af6894108fc2
Jun Nishimura <n.junjun0303@gmail.com>
2023-09-24 21:24:04 +0900

    update controller (#25)

--- adapter/controller/article.go
+++ adapter/controller/article.go
@@ -70,6 +70,33 @@ func (a *article) Create(w http.ResponseWriter, r *http.Request) {
 	inputPort.Create(r.Context(), newArticle)
 }
 
+func (a *article) Update(w http.ResponseWriter, r *http.Request) {
+	outputPort := presenter.NewArticle(w)
+	repository := gateway.NewArticleRepository(a.db)
+	inputPort := interactor.NewArticle(outputPort, repository)
+
+	strArticleID := chi.URLParam(r, "articleID")
+	articleID, err := strconv.ParseInt(strArticleID, 10, 64)
+	if err != nil {
+		outputPort.Render(r.Context(), &presenter.ErrResponse{
+			Message: fmt.Sprintf("invalid article id '%s'", strArticleID),
+		}, http.StatusBadRequest)
+		return
+	}
+
+	var b struct {
+		Title *string `json:"title,omitempty"`
+		Body  *string `json:"body,omitempty"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
+		outputPort.Render(r.Context(), &presenter.ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+	inputPort.Update(r.Context(), articleID, b.Title, b.Body)
+}
+
 func (a *article) Delete(w http.ResponseWriter, r *http.Request) {
 	outputPort := presenter.NewArticle(w)
 	repository := gateway.NewArticleRepository(a.db)
